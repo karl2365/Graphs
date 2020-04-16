2425e352086116569a016c41aef200e85a24a726
karl2365 <karlbeksa@gmail.com>
2020-04-16 17:19:38 -0400

    Tuesday MVP

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,43 +1,25 @@
 
-
-def createGraph(ancestor):
-    vertices = {}
-    for i in ancestor:
-        if i[1] not in vertices:
-            vertices[i[1]] = set()
-        if i[0] not in vertices:
-            vertices[i[0]] = set()
-        vertices[i[1]].add(i[0])
-    return vertices
-
-
-def earliest_ancestor(ancestor, starting_node):
-    a = createGraph(ancestor)
-    v = [False]*(len(a)+1)
-    s = []
-    s.append(starting_node)
-    p = []
-    sv = starting_node
-
-    while s:
-        starting_node = s[-1]
-        s.pop()
-        if (not v[starting_node]):
-            p.append(starting_node)
-            v[starting_node] = True
-        for node in a[starting_node]:
-            if (not v[node]):
-                s.append(node)
-
-    if sv == p[-1]:
+def earliest_ancestor(ancestors, starting_node):
+    stack = []
+    visited = set()
+    stack.append([starting_node])  # [ [2] ]
+    maxList = []
+    while len(stack) > 0:
+        path = stack.pop()  # [2]
+        v = path[-1]  # 2
+        if v not in visited:
+            visited.add(v)  # { 2 }
+            for neighbors in ancestors:
+                if v == neighbors[-1]:  # 2 != neighbors.. False
+                    current = [*path, neighbors[0]]
+                    if len(current) > len(maxList):
+                        maxList = [*current]
+                    if len(current) == len(maxList):
+                        if current[-1] < maxList[-1]:
+                            maxList = [*current]
+                    stack.append(current)
+    # since line 13 is False, maxList never gets updated since it doesnt have a parent.
+    if len(maxList) == 0:
         return -1
-    else:
-        if p[-1] not in a[p[-2]] and p[-2] < p[-1]:
-            return p[-2]
-        else:
-            return p[-1]
+    return maxList[-1]
 
-
-test = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
-        (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-print(earliest_ancestor(test, 6))
